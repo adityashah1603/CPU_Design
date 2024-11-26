1d8c98bf0de6e0f487b97f79de9039c752df46b4
Randy Nguyen <randy.nguyen01@sjsu.edu>
2024-11-26 10:04:26 -0800

    Created getInstruction function for InstructionMemory class

--- IMem.cpp
+++ IMem.cpp
@@ -1,5 +1,28 @@
+#include <iostream>
+#include <string>
 #include "IMem.h"
 
+using std::getline;
+using std::string;
+using std::stoi;
+using std::cerr;
+using std::endl;
+
 uint16_t InstructionMemory::getInstruction(const uint16_t &address) {
-    
+    uint16_t instruction {0b0};
+    string FileLine;
+
+    if(InstructionFile.is_open()) {
+        InstructionFile.seekg(std::ios::beg);
+        for(int line {0}; line <= address; line++) {
+            getline(InstructionFile, FileLine);
+        }
+
+        instruction = stoi(FileLine,0,16);
+    }
+    else {
+        cerr << "Error reading instruction. Instruction File not specified!" << endl;
+    }
+
+    return instruction;
 }
