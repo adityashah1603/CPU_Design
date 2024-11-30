588b8842bdde587725bbcc8a06083f370ec7e08d
Randy Nguyen <randy.nguyen01@sjsu.edu>
2024-11-29 19:57:48 -0800

    Simplified DataMemory by removing DataFile

--- DMem.h
+++ DMem.h
@@ -1,20 +1,15 @@
 #pragma once
-#include <fstream>
 #include <vector>
 #include <cstdint>
 
-using std::ofstream;
 using std::vector;
 
 class DataMemory {
 public:
     DataMemory(); // Default Constructor
-    DataMemory(const char* filename); // Constructor for provided Data File
-    ~DataMemory() { DataFile.close(); } // Destructor
     uint16_t getData(const uint16_t &address);
     void writeDataToMemory(const uint16_t &address, const uint16_t &writeData);
 private:
-    ofstream DataFile;
     vector<uint16_t> data;
     bool we_dm;
 };
