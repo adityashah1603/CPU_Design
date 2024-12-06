dff1ee38ba817a8a65f9e0658de056e46946a371
kavin-15 <kavin_1580@yahoo.com>
2024-12-05 16:35:23 -0800

    ALU file error fix

--- ALU.cpp
+++ ALU.cpp
@@ -7,6 +7,10 @@
 
 #include <cstdint>
 #include <limits>
+#include<iostream>
+
+using std::cerr;
+using std::endl;
 
 
 void compute(int func, int16_t &op1, int16_t op2, int16_t& result, bool& overflow, bool &zero) {
@@ -23,12 +27,9 @@ void compute(int func, int16_t &op1, int16_t op2, int16_t& result, bool& overflo
             break;
         case 1: // subtract
             result = op1-op2;
-            if (op2 > 0 && op1 < (std::numeric_limits<int16_t>::max() + op2)) {
-                overflow = 1; // Positive overflow
-            }
-            if (op2 < 0 && op1 > (std::numeric_limits<int16_t>::min() + op2)) {
-                overflow = 1; // Negative overflow
-            }
+            if ((op2 > 0 && op1 < std::numeric_limits<int16_t>::min() + op2) || (op2 < 0 && op1 > std::numeric_limits<int16_t>::max() + op2)) {
+                    overflow = 1;
+                    }
             break;
         case 2: // or
             result = op1 | op2;
@@ -47,8 +48,15 @@ void compute(int func, int16_t &op1, int16_t op2, int16_t& result, bool& overflo
             overflow = 0;
             break;
         case 7: // div
+            if (op2==0)
+            {
+                cerr<< "Division by zero"<<endl;
+                result = 0;
+                overflow = 1;
+            }
+            else{            
             result = op1 / op2;
-            overflow = 0;
+            overflow = 0;}
             break;
         case 8: // slt
             result = op1 < op2;
@@ -63,12 +71,18 @@ void compute(int func, int16_t &op1, int16_t op2, int16_t& result, bool& overflo
             overflow = 0;
             break;
         case 11: // sll
-            result = op1 << op2;
+            result = op1 << (op2 & 0xF);
             overflow = 0;
             break;
         case 12: // srl
-            result = op1 >> op2;
+            result = op1 >> (op2 & 0xF);
             overflow = 0;
+            break;
+        default:
+            std::cerr << "Error: Invalid ALU function code: " << func << std::endl;
+            result = 0;
+            overflow = 0;
+            zero = true;
             break;
     }
     zero = !result;
